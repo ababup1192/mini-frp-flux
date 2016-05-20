7c8848b4dee7496fe06bfb385603c753d3240dab
ababup1192 <ababup1192@gmail.com>
2016-05-21 06:15:41 +0900

    Modify refuctoring

--- src/scripts/actionCreator.ts
+++ src/scripts/actionCreator.ts
@@ -3,25 +3,25 @@ import {Dispatcher} from "./dispatcher";
 import {Constant} from "./constant";
 
 export class ActionCreator {
-    private dispatcher: Dispatcher;
+    private d: Dispatcher;
 
     constructor(dispatcher: Dispatcher) {
-        this.dispatcher = dispatcher;
+        this.d = dispatcher;
     }
 
     public countUp(): void {
-        this.dispatcher.push(Constant.COUNT_UP, undefined);
+        this.d.push(Constant.COUNT_UP, undefined);
     }
 
     public countDown(): void {
-        this.dispatcher.push(Constant.COUNT_DOWN, undefined);
+        this.d.push(Constant.COUNT_DOWN, undefined);
     }
 
     public createProperty(initialValue: number): Bacon.Property<any, any> {
         const countUpStream: Bacon.EventStream<number, number> =
-            this.dispatcher.stream(Constant.COUNT_UP).map((_: any) => +1);
+            this.d.stream(Constant.COUNT_UP).map((_: any) => +1);
         const countDownStream: Bacon.EventStream<number, number> =
-            this.dispatcher.stream(Constant.COUNT_DOWN).map((_: any) => -1);
+            this.d.stream(Constant.COUNT_DOWN).map((_: any) => -1);
 
         return countUpStream.merge(countDownStream).
             scan<number>(initialValue, (acc: number, next: number) => acc + next);
